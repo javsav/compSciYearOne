185bb773c9e6f21a825cbd36e6aac5cff88a0606
javsav <javed.alam@student.adelaide.edu.au>
2023-05-17 17:52:58 +0930

    Delete Practical 3 directory

--- Practical 3/Question_2/q2.m
+++
@@ -1,6 +0,0 @@
-% For this problem you have to create a program that defines two vectors of numbers: 
-% A and B and uses a for-loop to combine the values of A and B. 
-% Note that different parts of the question below specify different orders for combining the elements.
-
-A=[1:10]
-B=[11:20]
--- Practical 3/Question_3a/Drafts/q3a.m
+++
@@ -1,6 +0,0 @@
-% This is a simple program that reads a number as input and prints out 'Divisible by four' if that number is divisible by four
-
-fprintf("This program checks if a number is divisible by four.\n")
-number = input("Please enter the number which you wish to check divisibility by four of: ")
-while number == 0 || ~isa(number, 'double')
-    number = input("Please enter a numerical value other than zero")
