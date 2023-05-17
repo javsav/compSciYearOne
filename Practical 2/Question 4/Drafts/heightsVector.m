71c183b042fd41fb2036385514d622c4f33b48a8
javsav <javed.alam@student.adelaide.edu.au>
2023-05-17 17:52:40 +0930

    Delete Practical 2 directory

--- Practical 2/Question 4/Drafts/heightsVector.m
+++
@@ -1,5 +0,0 @@
-% This program reads in the inputs of heights of 5 students in centimetres,
-% stores them in a vector and then displays the output of each height on a
-% new line.
-
-heights = zeros([1 5])
