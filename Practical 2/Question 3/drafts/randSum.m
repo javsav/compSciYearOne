cbef6cbe2d550515ce04c2ead69141d37fc46bbd
javsav <javed.alam@student.adelaide.edu.au>
2023-05-17 14:38:22 +0930

    Added MATLAB Practical 2

---
+++ Practical 2/Question 3/drafts/randSum.m
@@ -0,0 +1,11 @@
+% This is a program that generates and sums five random integers between 1
+% and 10.
+
+% Declares empty result variable
+
+result = 0
+
+% For loop to generate five random integers from 1 to 10 inclusive
+
+for range = 1:5
+    result = result +  
---
+++ Practical 2/Question 4/Drafts/heightsVector2.m
@@ -0,0 +1,14 @@
+% This program reads in the inputs of heights of 5 students in centimetres,
+% stores them in a vector and then displays the output of each height on a
+% new line.
+
+% Creates 1 * 5 vector of zeroes
+
+heights = zeros([1 5])
+
+for index = 1:5
+    fprintf("Enter height of student %i (cm): ", index)
+    heights(index) = input("");
+end
+
+disp(heights)
