20fb82bfe339ff8ff38513fc93c5821a273c3192
javsav <javed.alam@student.adelaide.edu.au>
2023-05-17 19:44:26 +0930

    Added first script of C Practical 8

---
+++ C Practicals/Practical 8/temperatures01.c
@@ -0,0 +1,50 @@
+/* This program receives an input of n temperatures (specified by the user),
+ * obtains each of the n temperatures as floats, and creates a dynamic array in
+ * the heap, with a memory size of n * the size of a float. It then appends
+ * these characters to an array, and uses a for loop to iterate from the final
+ * element of the array (n - 1th element) to the 0th element, and prints each
+ * temperature in the reverse order that they were entered, with 1 significant
+ * digit of accuracy.
+ */
+
+#include <stdio.h>   // For scanf and printf
+#include <stdlib.h>  // For malloc and free
+
+int main() {
+  // numberOfTemps var determines num temps to input & memory allocation size.
+  int numberOfTemps = 0;
+  // Float pointer to tempArray.
+  float *tempArray;
+
+  // Ask the user for the number of temperatures
+  printf("Please enter the number of temperatures that you wish to input: ");
+  scanf("%d", &numberOfTemps);
+
+  // define tempArray using float pointer,allocate memory for usr numberOfTemps.
+  tempArray = (float *)malloc(numberOfTemps * sizeof(float));
+  // Check if malloc has properly allocated memory for numberofTemps in heap.
+  if (tempArray == NULL) {
+    printf("Memory allocation failed!\n");
+    return 1;  // Indicate unsuccessful execution and terminate.
+  }
+
+  /* Read in temps up to numberofTemps and append each to the numberofTempsth
+   * index of tempArray */
+  for (int tempIndex = 0; tempIndex < numberOfTemps; tempIndex++) {
+    printf("Enter temperature %d (degrees Celsius): ", tempIndex + 1);
+    scanf("%f", &tempArray[tempIndex]);
+  }
+
+  printf("\nYou entered the following temperatures:\n");
+  // Iterate through tempArray from the final index to the 0th index, step -1.
+  for (int tempIndex = numberOfTemps - 1; tempIndex > -1; tempIndex--) {
+    // Print each temperature with one decimal place.
+    printf("%.1f ", tempArray[tempIndex]);
+  }
+  printf("\n");
+
+  // Free memory allocated by malloc to tempArray
+  free(tempArray);
+
+  return 0;  // Indicate successful execution and terminate.
+}
